11008258ef239730bfc7f5e13281f3f2c6076245
jochoa70 <jocho70@wgu.edu>
2024-04-08 15:25:07 -0700

    add CSV files33

--- app.py
+++ app.py
@@ -1,15 +1,32 @@
-from flask import Flask, render_template
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import sklearn
+from sklearn.model_selection import train_test_split
+from sklearn import datasets
+from sklearn import svm
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
 
+houses = pd.read_csv('Walmart_Store_sales.csv')
+
+
+
+houses.columns
+from flask import Flask, render_template, url_for
+import requests
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
-
 def index(): 
     return render_template('index.html')
+
+
 if __name__ == "__main__": 
     app.run(debug=True)
-
 
 
 
